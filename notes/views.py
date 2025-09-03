90877a1c5d3f055b4ead520a99fb6fc89889fd97
jml-moe <2318069@student.itn.ac.id>
2025-09-03 16:22:33 +0700

    Add initial templates and views for hobby list feature

--- notes/views.py
+++ notes/views.py
@@ -2,4 +2,22 @@ from django.shortcuts import render
 
 # Create your views here.
 def index_view(request):
-    return render(request, 'index.html')
+
+    # context = {
+    #     "username": "Jamil",
+    #     "email": "jamil@me.com"
+    # } # -> bentuknya dict, bisa berupa third-party api atau database dll.
+
+    my_hobbies = ["reading", "coding", "sleeping"]
+
+    # return render(request, 'index.html', context) # -> fungsi render() hanya menerima satu parameter
+    return render(request, 'index.html', {"hobbies": my_hobbies}) # -> hobbies itu nama key-nya
+
+    # Solusi penggabungan data kedalam satu dict.
+    
+    # context = {
+    # "username": "Jamil",
+    # "email": "jamil@me.com",
+    # "my_hobbies": ["reading", "coding", "sleeping"]
+    # }
+    # return render(request, 'index.html', context)
