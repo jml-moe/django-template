935e91d226adfd45b5a4e9cc1cf78662b7cd2353
jml-moe <2318069@student.itn.ac.id>
2025-09-03 17:32:45 +0700

    Implement notes feature with model, views, and templates; add static files configuration

--- notes/models.py
+++ notes/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Notes(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.title
--- notes/views.py
+++ notes/views.py
@@ -1,17 +1,25 @@
 from django.shortcuts import render
+from .models import Notes
 
 # Create your views here.
 def index_view(request):
+    notes = Notes.objects.all()
+
+    context = {
+        "notes": notes
+    }
+
+    return render(request, 'index.html', context)
 
     # context = {
     #     "username": "Jamil",
     #     "email": "jamil@me.com"
     # } # -> bentuknya dict, bisa berupa third-party api atau database dll.
 
-    my_hobbies = ["reading", "coding", "sleeping"]
+    # my_hobbies = ["reading", "coding", "sleeping"]
 
-    # return render(request, 'index.html', context) # -> fungsi render() hanya menerima satu parameter
-    return render(request, 'index.html', {"hobbies": my_hobbies}) # -> hobbies itu nama key-nya
+    # # return render(request, 'index.html', context) # -> fungsi render() hanya menerima satu parameter
+    # return render(request, 'index.html', {"hobbies": my_hobbies}) # -> hobbies itu nama key-nya
 
     # Solusi penggabungan data kedalam satu dict.
     
